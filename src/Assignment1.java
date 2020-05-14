d62d8f23398a10bdea533da5d74c435f81302783
Joe <jeric720@icloud.com>
2020-05-14 17:52:29 -0400

    Initial Commit and Completed Assignment 1 - Two Object Concatenation

---
+++ src/Assignment1.java
@@ -0,0 +1,21 @@
+
+/*
+ * 1.	Write a class named Assignment1 that declares two strings, “Hello” and (your name) and concatenates 
+ * 		them together. Print the result.
+
+ */
+
+
+public class Assignment1 {
+	
+	public static void main(String[] args) {
+		
+		String a = "Hello";
+		String b = " World";
+				
+		System.out.println(a + b);
+		
+		
+	}
+
+}
