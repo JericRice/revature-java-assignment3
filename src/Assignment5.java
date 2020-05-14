3fad65d60e7c45d20fafa69d66902b6c75b520a9
Joe <jeric720@icloud.com>
2020-05-14 18:41:59 -0400

    Completed Assignment 5 - Substring printing

---
+++ src/Assignment5.java
@@ -0,0 +1,14 @@
+
+/*
+ * Write a class named Assignment5 that uses the substring() method to print the last 5 characters of "Hello My Name is Java".
+ */
+
+public class Assignment5 {
+
+	public static void main(String[] args) {
+		
+		String str = new String("Hello My Name is Java");
+		
+		System.out.println(str.substring(16));
+	}
+}
