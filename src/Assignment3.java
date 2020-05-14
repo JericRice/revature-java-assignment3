d9271d22080a6b97160b3d3c3ee8ab69f06d5a97
Joe <jeric720@icloud.com>
2020-05-14 18:19:23 -0400

    Completed Assignment 3 - Comparison of String objects

---
+++ src/Assignment3.java
@@ -0,0 +1,26 @@
+/*
+ * Write a class named Assignment3 that declares 2 strings “abc” and “abc”. 
+ * 
+ * The first should use the string literal syntax and the other should use the new keyword. 
+ * 
+ * Write a comparison operator that prints a message if they are equal or otherwise. (Use an if/else-statement)
+ */
+
+
+public class Assignment3 {
+	
+	public static void main(String[] args) {
+		
+		String a = "abc";		
+		String b = new String("abc");
+
+		
+		if (a == b) {
+			System.out.println("The strings are the same");
+		} else {
+			System.out.println("The strings are not the same");
+		}
+		
+	}
+
+}
